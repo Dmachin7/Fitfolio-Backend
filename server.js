79f734fe14349af5fe70305a241332a69121ba25
Daniel Machin <DanielTAYG@gmail.com>
2023-12-03 14:01:21 -0500

    added user model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const bcrypt = require('bcrypt')
+const methodOverride = require('method-override')
 
 require('dotenv').config()
 
