e2377229f03f1310cd990a9a8dd6ac42d0469c17
Daniel Machin <DanielTAYG@gmail.com>
2023-12-09 13:43:25 -0500

    workout post and get route work, also user register route works

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+/////////////////////////////
+// DEPENDENCIES
+////////////////////////////
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const session = require('express-session')
 const bcrypt = require('bcrypt')
 const methodOverride = require('method-override')
-
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
 
+//////////////////////////////
+// MIDDLEWARE
+//////////////////////////////
+const PORT = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 app.use(
@@ -18,9 +26,19 @@ app.use(
     })
 )
 
-userController = require('./controllers/user.js')
+///////////////////////////////
+// CONTROLLERS 
+///////////////////////////////
 
-// setup database 
+// USER CONTROLLER
+const userController = require('./controllers/user.js')
+
+// WORKOUTS CONTROLLER
+const workoutsController = require('./controllers/workouts.js')
+
+//////////////////////////////
+// DATABASE CONNECTION
+//////////////////////////////
 const mongoose = require('mongoose')
 const mongoURI = process.env.MONGO_URI
 
@@ -28,18 +46,28 @@ const mongoURI = process.env.MONGO_URI
 mongoose.connect(mongoURI)
 
 const db = mongoose.connection
+
 // optional create status messages to check mongo connection 
 db.on('error', (err) => { console.log('ERROR: ' , err)})
 db.on('connected', () => { console.log('mongo connected')})
 db.on('disconnected', () => { console.log('mongo disconnected')})
 
+////////////////////////////////
+// ROUTES
+////////////////////////////////
+
+// ROUTE TO USER CONTROLLER
 app.use('/user', userController )
 
+// ROUTE TO WORKOUTS CONTROLLER
+app.use('/workouts', workoutsController)
+
+// BASE GET ROUTE
 app.get('/', (req,res) => {
     res.send("Mean Stack Time!")
 })
 
-
+// BASE LISTEN ROUTE
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
